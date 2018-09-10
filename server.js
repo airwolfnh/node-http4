7fd2ae6e6763ace8ed089d636ceed1c544b91cd2
airwolfnh <airwolfnh>
2018-09-10 10:41:09 +0100

    Adding new

--- server.js
+++ server.js
@@ -1,21 +1,83 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = process.env.MONGODB_URI ;
+
+var insertDocument = function(db, callback) {
+db.collection('families').insertOne( {
+        "id": "AndersenFamily",
+        "lastName": "Andersen",
+        "parents": [
+            { "firstName": "Thomas" },
+            { "firstName": "Mary Kay" }
+        ],
+        "children": [
+            { "firstName": "John", "gender": "male", "grade": 7 }
+        ],
+        "pets": [
+            { "givenName": "Fluffy" }
+        ],
+        "address": { "country": "USA", "state": "WA", "city": "Seattle" }
+    }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the families collection.");
+    callback();
+});
+};
+
+var findFamilies = function(db, callback) {
+var cursor =db.collection('families').find( );
+cursor.each(function(err, doc) {
+    assert.equal(err, null);
+    if (doc != null) {
+        console.dir(doc);
+    } else {
+        callback();
+    }
+});
+};
+
+var updateFamilies = function(db, callback) {
+db.collection('families').updateOne(
+    { "lastName" : "Andersen" },
+    {
+        $set: { "pets": [
+            { "givenName": "Fluffy" },
+            { "givenName": "Rocky"}
+        ] },
+        $currentDate: { "lastModified": true }
+    }, function(err, results) {
+    console.log(results);
+    callback();
+});
+};
+
+var removeFamilies = function(db, callback) {
+db.collection('families').deleteMany(
+    { "lastName": "Andersen" },
+    function(err, results) {
+        console.log(results);
+        callback();
+    }
+);
+};
 
 var http = require('http');
-var fileSystem = require('fs');
 
-var server = http.createServer(function(req, resp){
-	fileSystem.readFile('./index.html', function(error, fileContent){
-		if(error){
-			resp.writeHead(500, {'Content-Type': 'text/plain'});
-			resp.end('Error');
-		}
-		else{
-			resp.writeHead(200, {'Content-Type': 'text/html'});
-			resp.write(fileContent);
-			resp.end();
-		}
-	});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Welcome');
+  MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  var db = client.db('familiesdb');
+  insertDocument(db, function() {
+      findFamilies(db, function() {
+      updateFamilies(db, function() {
+         removeFamilies(db, function() {
+              client.close();
+          });
+      });
+      });
+  });
 });
-
-server.listen(8080);
-
-
+}).listen(8080);
